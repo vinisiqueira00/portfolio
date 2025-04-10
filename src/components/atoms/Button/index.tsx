9dd7c4bb389cc60d7643726c5648147105dfb74f
Vinícius Siqueira <vinicius.siqueira@cadastra.com>
2025-04-09 23:44:24 -0300

    feat: implementa envio do form de contato ao backend

--- src/components/atoms/Button/index.tsx
+++ src/components/atoms/Button/index.tsx
@@ -6,6 +6,7 @@ interface SubmitButtonProps {
   label: string;
   type: "primary" | "secondary";
   onClick: () => void;
+  isLoading?: boolean;
 }
 
 interface LinkButtonProps {
@@ -13,6 +14,7 @@ interface LinkButtonProps {
   type: "primary" | "secondary";
   link: string;
   target?: "_self" | "_blank";
+  isLoading?: boolean;
 }
 
 function Button(props: SubmitButtonProps | LinkButtonProps) {
@@ -24,17 +26,21 @@ function Button(props: SubmitButtonProps | LinkButtonProps) {
         target={props.target ?? "_self"}
         className="group flex items-center justify-center w-auto h-14 px-6 rounded-2xl leading-normal text-base font-normal data-[type=primary]:text-light-neutral-000 data-[type=primary]:dark:text-dark-neutral-000 data-[type=secondary]:text-light-neutral-900 data-[type=secondary]:dark:text-dark-neutral-900 data-[type=primary]:bg-light-gradient-004 dark:data-[type=primary]:bg-dark-gradient-004 data-[type=secondary]:bg-transparent data-[type=secondary]:border data-[type=secondary]:border-light-neutral-400 dark:data-[type=secondary]:border-dark-neutral-400"
       >
-        <div className="relative w-auto h-auto p-0 m-0 overflow-hidden">
-          <span className="opacity-0 pointer-events-none select-none overflow-hidden">
-            {props.label}
-          </span>
-          <span className="absolute 0 left-1/2 -translate-x-1/2 translate-y-0 w-full h-auto group-hover:-translate-y-full transition-transform">
-            {props.label}
-          </span>
-          <span className="absolute 0 left-1/2 -translate-x-1/2 translate-y-full w-full h-auto group-hover:translate-y-0 transition-transform">
-            {props.label}
-          </span>
-        </div>
+        {props.isLoading ? (
+          <span className="w-4 h-4 rounded-full border group-data-[type=primary]:border-light-neutral-000 dark:group-data-[type=primary]:border-dark-neutral-000 group-data-[type=secondary]:border-light-neutral-900 dark:group-data-[type=secondary]:border-dark-neutral-900 animate-spin" />
+        ) : (
+          <div className="relative w-auto h-auto p-0 m-0 overflow-hidden">
+            <span className="opacity-0 pointer-events-none select-none overflow-hidden">
+              {props.label}
+            </span>
+            <span className="absolute 0 left-1/2 -translate-x-1/2 translate-y-0 w-full h-auto group-hover:-translate-y-full transition-transform">
+              {props.label}
+            </span>
+            <span className="absolute 0 left-1/2 -translate-x-1/2 translate-y-full w-full h-auto group-hover:translate-y-0 transition-transform">
+              {props.label}
+            </span>
+          </div>
+        )}
       </Link>
     );
   }
@@ -47,17 +53,21 @@ function Button(props: SubmitButtonProps | LinkButtonProps) {
         aria-label={props.label}
         className="group flex items-center justify-center w-auto h-14 px-6 rounded-2xl leading-normal text-base font-normal data-[type=primary]:text-light-neutral-000 data-[type=primary]:dark:text-dark-neutral-000 data-[type=secondary]:text-light-neutral-900 data-[type=secondary]:dark:text-dark-neutral-900 data-[type=primary]:bg-light-gradient-004 dark:data-[type=primary]:bg-dark-gradient-004 data-[type=secondary]:bg-transparent data-[type=secondary]:border data-[type=secondary]:border-light-neutral-400 dark:data-[type=secondary]:border-dark-neutral-400"
       >
-        <div className="relative w-auto h-auto p-0 m-0 overflow-hidden">
-          <span className="opacity-0 pointer-events-none select-none overflow-hidden">
-            {props.label}
-          </span>
-          <span className="absolute 0 left-1/2 -translate-x-1/2 translate-y-0 w-full h-auto group-hover:-translate-y-full transition-transform">
-            {props.label}
-          </span>
-          <span className="absolute 0 left-1/2 -translate-x-1/2 translate-y-full w-full h-auto group-hover:translate-y-0 transition-transform">
-            {props.label}
-          </span>
-        </div>
+        {props.isLoading ? (
+          <span className="w-4 h-4 rounded-full border group-data-[type=primary]:border-light-neutral-000 dark:group-data-[type=primary]:border-dark-neutral-000 group-data-[type=secondary]:border-light-neutral-900 dark:group-data-[type=secondary]:border-dark-neutral-900 animate-spin" />
+        ) : (
+          <div className="relative w-auto h-auto p-0 m-0 overflow-hidden">
+            <span className="opacity-0 pointer-events-none select-none overflow-hidden">
+              {props.label}
+            </span>
+            <span className="absolute 0 left-1/2 -translate-x-1/2 translate-y-0 w-full h-auto group-hover:-translate-y-full transition-transform">
+              {props.label}
+            </span>
+            <span className="absolute 0 left-1/2 -translate-x-1/2 translate-y-full w-full h-auto group-hover:translate-y-0 transition-transform">
+              {props.label}
+            </span>
+          </div>
+        )}
       </button>
     );
   }
--- src/components/molecules/ContactForm/index.tsx
+++ src/components/molecules/ContactForm/index.tsx
@@ -4,20 +4,58 @@ import { useTranslations } from "next-intl";
 
 import { Button } from "@/components/atoms/Button";
 import { ContactInput } from "@/components/atoms/ContactInput";
+import { FormEvent, useState } from "react";
 
 function ContactForm() {
   const t = useTranslations("");
 
-  function handleSubmit() {}
+  const [value, setValue] = useState<string>("");
+  const [status, setStatus] = useState<"LOADING" | "ERROR" | "SUCCESS">();
+
+  async function handleSubmit(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    event.stopPropagation();
+
+    if (value.trim().length === 0) return;
+
+    try {
+      setStatus("LOADING");
+
+      const response = await fetch(
+        "https://portfolio-backend-cmoh.onrender.com/contact",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ message: value }),
+        }
+      );
+
+      if (response.status !== 201) throw new Error();
+
+      setValue("");
+      setStatus("SUCCESS");
+    } catch (err) {
+      console.error(err);
+
+      setStatus("ERROR");
+    }
+  }
 
   return (
     <form
       id="contact"
-      onSubmit={() => handleSubmit()}
+      onSubmit={(e) => handleSubmit(e)}
       className="flex flex-col items-stretch justify-center gap-2 w-full h-auto p-2 rounded-2xl border border-light-neutral-500 dark:border-dark-neutral-500 md:flex-row md:max-w-[28rem]"
     >
-      <ContactInput placeholder={t("contact-placeholder")} />
+      <ContactInput
+        placeholder={t("contact-placeholder")}
+        value={value}
+        setValue={setValue}
+      />
       <Button
+        isLoading={status === "LOADING"}
         label={t("contact-button-submit")}
         type="primary"
         onClick={() => {}}
